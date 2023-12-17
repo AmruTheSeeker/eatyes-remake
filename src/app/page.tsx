6ebfca9c5e6a8df741a9a368c4a4864a49069c0f
Jessy Hanifiah <jessyhanifiah@gmail.com>
2023-12-17 20:56:32 +0700

    feat: add login ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { signIn } from 'next-auth/react';
+import { Login } from '@/components/Auth/Login';
 
 export default function Home() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-4">
-      <h1 className=" text-4xl font-bold">Next.js Starter Template</h1>
-      <p>by @jeeehaan</p>
-      <Button onClick={() => signIn()}>Login</Button>
-    </div>
-  );
+  return <Login />;
 }
